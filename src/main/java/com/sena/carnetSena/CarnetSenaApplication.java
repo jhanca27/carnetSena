bf1d684a6db3fd532763b90a0f690ed79870030a
jhanca <jhancarlohigueracarvajal@gmail.com>
2023-09-22 10:15:02 -0500

    creacion proyecto

---
+++ src/main/java/com/sena/carnetSena/CarnetSenaApplication.java
@@ -0,0 +1,13 @@
+package com.sena.carnetSena;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarnetSenaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CarnetSenaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sena/carnetSena/CarnetSenaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sena.carnetSena;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarnetSenaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
